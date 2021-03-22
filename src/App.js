9e1ae026529026a30ff06a4db14dee8a7610c668
Gaurav <aadeezcity@gmail.com>
2021-03-22 17:33:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CakeContainer from "./compontents/CakeContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
